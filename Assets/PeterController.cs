0b90e8e00b4eec05527dd0c1be26c0bb8b618576
mashtoe <mashtoe@hotmail.com>
2018-05-04 14:32:21 +0200

    collision

--- Assets/PeterController.cs
+++ Assets/PeterController.cs
@@ -15,20 +15,17 @@ public class PeterController : MonoBehaviour
 
     private GameObject bookBullet;
     private GameObject target;
+    private Transform bookSpawnPoint;
 
+    private bool readyToStartCoroutine;
 
 
 	void Start () {
+        readyToStartCoroutine = true;
         target = FindObjectOfType<PlacyerController>().gameObject;
 	}
 	
 	void Update () {
-
-	    if (Input.GetKeyDown(KeyCode.Space))
-	    {
-	        StartCoroutine(ThrowBook());
-	    }
-
         if (target == null)
         {
             target = FindObjectOfType<PlacyerController>().gameObject;
@@ -39,24 +36,20 @@ public class PeterController : MonoBehaviour
             FollowPlayer();
 
         }
+
+        if (readyToStartCoroutine)
+        {
+            StartCoroutine(PeterRoutine());
+        }
     }
 
     void Awake ()
     {
         anim = transform.Find("Peter").GetComponent<Animator>();
         bookBullet = Resources.Load("BookBullet") as GameObject;
-    }
+        bookSpawnPoint = transform.Find("Peter").Find("Hand L Pivot").Find("Hand L").Find("BookSpawnPoint");
 
-    IEnumerator ThrowBook()
-    {
-        anim.SetTrigger("attackTrigger");
-        yield return new WaitForSeconds(COUNTER);
-        Instantiate(bookBullet, transform.position, transform.rotation);
-        transform.Find("Peter").Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(false);
-        yield return new WaitForSeconds(COUNTER);
-        transform.Find("Peter").Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(true);
     }
-
 
     void FollowPlayer()
     {
@@ -85,4 +78,25 @@ public class PeterController : MonoBehaviour
         }
         return false;
     }
+
+    IEnumerator PeterRoutine()
+    {
+        readyToStartCoroutine = false;
+
+        StartCoroutine(ThrowBook());
+        yield return new WaitForSeconds(1.5f);
+
+        readyToStartCoroutine = true;
+    }
+
+    IEnumerator ThrowBook()
+    {
+        anim.SetTrigger("attackTrigger");
+        yield return new WaitForSeconds(COUNTER);
+        Instantiate(bookBullet, bookSpawnPoint.position, transform.rotation);
+        transform.Find("Peter").Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(false);
+        yield return new WaitForSeconds(COUNTER);
+        transform.Find("Peter").Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(true);
+    }
+
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Projectile : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log(collider.gameObject);
+
+    }
+}
