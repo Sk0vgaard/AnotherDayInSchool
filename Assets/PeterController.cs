83233442375a4cb14b62d84a8def299601cf36d4
mashtoe <mashtoe@hotmail.com>
2018-05-04 13:51:36 +0200

    peter move functionality

--- Assets/PeterController.cs
+++ Assets/PeterController.cs
@@ -6,47 +6,83 @@ using UnityEngine;
 
 public class PeterController : MonoBehaviour
 {
-    private Animator anim;
-    //private float counter;
-    private static readonly float COUNTER = 0.42f;
-    private GameObject bookBullet;
-    //private bool countDown;
 
-	// Use this for initialization
+    public float moveSpeed;
+
+    private Animator anim;
+    private static readonly float COUNTER = 0.42f;
+    private static readonly float MOVE_THRESHHOLD = 0.3f;
+
+    private GameObject bookBullet;
+    private GameObject target;
+
+
+
 	void Start () {
-		
+        target = FindObjectOfType<PlacyerController>().gameObject;
 	}
 	
-	// Update is called once per frame
 	void Update () {
 
 	    if (Input.GetKeyDown(KeyCode.Space))
 	    {
 	        StartCoroutine(ThrowBook());
-	        //Attack();
 	    }
-        
-	}
+
+        if (target == null)
+        {
+            target = FindObjectOfType<PlacyerController>().gameObject;
+        }
+
+        if (! (transform.position.y >= target.transform.position.y - MOVE_THRESHHOLD && transform.position.y <= target.transform.position.y + MOVE_THRESHHOLD))
+        {
+            FollowPlayer();
+
+        }
+    }
 
     void Awake ()
     {
-        anim = GetComponent<Animator>();
+        anim = transform.Find("Peter").GetComponent<Animator>();
         bookBullet = Resources.Load("BookBullet") as GameObject;
     }
 
     IEnumerator ThrowBook()
     {
-
         anim.SetTrigger("attackTrigger");
-    
         yield return new WaitForSeconds(COUNTER);
-
         Instantiate(bookBullet, transform.position, transform.rotation);
-        transform.Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(false);
-
+        transform.Find("Peter").Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(false);
         yield return new WaitForSeconds(COUNTER);
+        transform.Find("Peter").Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(true);
+    }
 
-        transform.Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(true);
 
+    void FollowPlayer()
+    {
+        if (PlayerIsAbove())
+        {
+            //go up
+            transform.Translate(Vector2.up * Time.deltaTime * moveSpeed);
+        }
+        else
+        {
+            //go down
+            transform.Translate(Vector2.down * Time.deltaTime * moveSpeed);
+
+        }
+    }
+
+    bool PlayerIsAbove()
+    {
+        if (target)
+        {
+            if (target.transform.position.y > transform.position.y)
+            {
+                return true;
+            }
+            
+        }
+        return false;
     }
 }
