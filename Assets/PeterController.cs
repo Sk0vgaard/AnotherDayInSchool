a9168181518b29bbf77ff21187915e62344c914c
Sk0vgaard <Sk0vgaard@live.dk>
2018-05-04 12:41:09 +0200

    Throw book

--- Assets/PeterController.cs
+++ Assets/PeterController.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System.CodeDom;
+using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Internal;
 using UnityEngine;
 
 public class PeterController : MonoBehaviour
 {
     private Animator anim;
+    //private float counter;
+    private static readonly float COUNTER = 0.42f;
+    private GameObject bookBullet;
+    //private bool countDown;
 
 	// Use this for initialization
 	void Start () {
@@ -13,20 +19,34 @@ public class PeterController : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
+
 	    if (Input.GetKeyDown(KeyCode.Space))
 	    {
-	        Attack();
+	        StartCoroutine(ThrowBook());
+	        //Attack();
 	    }
+        
 	}
 
     void Awake ()
     {
         anim = GetComponent<Animator>();
+        bookBullet = Resources.Load("BookBullet") as GameObject;
     }
 
-    
-    void Attack ()
+    IEnumerator ThrowBook()
     {
-        anim.SetTrigger("attackTrigger");    
+
+        anim.SetTrigger("attackTrigger");
+    
+        yield return new WaitForSeconds(COUNTER);
+
+        Instantiate(bookBullet, transform.position, transform.rotation);
+        transform.Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(COUNTER);
+
+        transform.Find("Hand L Pivot").Find("Hand L").Find("UnityBook").gameObject.SetActive(true);
+
     }
 }
