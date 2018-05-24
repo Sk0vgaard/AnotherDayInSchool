dd4e96aa81f32172867f7eb23319307f8c117d84
Sk0vgaard <Sk0vgaard@live.dk>
2018-05-24 13:42:32 +0200

    Doc + Refactor code.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,9 @@ public class PlayerController : HealthSystem {
     private Weapon currentWeapon;
     private bool flipped;
 
-
+    /// <summary>
+    /// Gets references to the components for the player.
+    /// </summary>
     new void Awake()
     {
         base.Awake();        
@@ -26,6 +28,9 @@ public class PlayerController : HealthSystem {
         animator.runtimeAnimatorController = character.animatorController;
     }
 
+    /// <summary>
+    /// Runs when the player has been instantiated.
+    /// </summary>
     new void Start () {
         base.Start();
         GameObject weaponGameObject = Instantiate(character.startingWeapon.gameObject, transform.position, transform.rotation) as GameObject;
@@ -36,11 +41,50 @@ public class PlayerController : HealthSystem {
         
     }
 	
-	void Update () {
+	void Update ()
+	{
+	    PlayerAnimator();
+
+	}
+
+    void FixedUpdate()
+    {
+        HandlePlayerActions();
+
+        rb.velocity = moveDirection * character.moveSpeed;
+    }
+
+    /// <summary>
+    /// When to move or not to move.
+    /// </summary>
+    private void HandlePlayerActions()
+    {
+        // if alive and able to move, move and shoot.
+        if (!disableMovements && !isDead)
+        {
+            Movement();
+            HandleShooting();
+            RotateWeapon();
+        }
+        // If dead or unable to move, stop movement and shooting.
+        else
+        {
+            moveDirection = new Vector2(0, 0);
+            if (currentWeapon != null)
+            {
+                currentWeapon.isPressingTrigger = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Two diffents animations, one to walk and another when standing still.
+    /// </summary>
+    private void PlayerAnimator()
+    {
         if (rb.velocity.x == 0 && rb.velocity.y == 0)
         {
             animator.SetFloat("speed", 0f);
-
         }
         else
         {
@@ -56,25 +100,6 @@ public class PlayerController : HealthSystem {
         {
             sr.flipX = false;
         }
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            disableMovements = !disableMovements;
-        }
-
-        if (!disableMovements && !isDead)
-        {
-            Movement();
-            HandleShooting();
-        }
-        else
-        {
-            moveDirection = new Vector2(0,0);
-            if (currentWeapon != null)
-            {
-                currentWeapon.isPressingTrigger = false;
-            }
-        }
     }
 
     void Movement()
@@ -82,6 +107,9 @@ public class PlayerController : HealthSystem {
         moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
     }
 
+    /// <summary>
+    /// When to shoot.
+    /// </summary>
     void HandleShooting()
     {
         if (Input.GetKey(KeyCode.Mouse0))
@@ -91,16 +119,18 @@ public class PlayerController : HealthSystem {
                 currentWeapon.isPressingTrigger = true;
             }
         }
-
-        if (!Input.GetKey(KeyCode.Mouse0))
+        else
         {
             if (currentWeapon != null)
             {
-                currentWeapon.isPressingTrigger = false;
+            currentWeapon.isPressingTrigger = false;
             }
         }
     }
 
+    /// <summary>
+    /// Rotates the gun around the player.
+    /// </summary>
     void RotateWeapon()
     {
         if (currentWeapon != null)
@@ -119,16 +149,12 @@ public class PlayerController : HealthSystem {
         }
     }
 
-    void FixedUpdate()
-    {
-        rb.velocity = moveDirection * character.moveSpeed;
-        if (!disableMovements && !isDead)
-        {
-            RotateWeapon();
 
-        }
-    }
-
+    /// <summary>
+    /// Used to make the gun rotate to follow the mouse.
+    /// </summary>
+    /// <param name="rotatatingObjTransform"></param>
+    /// <returns></returns>
     private Quaternion CalculateRotationToLookAtMouse(Transform rotatatingObjTransform)
     {
         Vector3 dir;
@@ -139,12 +165,14 @@ public class PlayerController : HealthSystem {
         return Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
+    /// <summary>
+    /// Run animation when player dies.
+    /// </summary>
     public override void Die()
     {
         isDead = true;
         GameObject bloodSplatter = Resources.Load("blood") as GameObject;
         Instantiate(bloodSplatter, transform.position, transform.rotation);
         transform.Find("Sprite").gameObject.SetActive(false);
-        //disableMovements = true;
     }
 }
